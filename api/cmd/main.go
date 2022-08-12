7b2345a9fefb4ae7ccd7e11a19aa4afa7b359e50
Ata Atasoy <h.ataatasoy@gmail.com>
2022-08-12 23:56:56 +0300

    Configure middleware

--- api/cmd/main.go
+++ api/cmd/main.go
@@ -7,8 +7,9 @@ import (
 	"api/pkg/track"
 	"net/http"
 
-	"github.com/gorilla/mux"
 	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
+
 	"github.com/joho/godotenv"
 )
 
@@ -16,12 +17,24 @@ func main() {
 	godotenv.Load(".env")
 
 	router := mux.NewRouter()
-	
+	router.Use(accessControlMiddleware)
+
 	router.HandleFunc("/", authentication.GetUserAuthorization)
 	router.HandleFunc("/callback", authentication.GetAccessToken)
-	router.HandleFunc("/me/favorite/tracks", track.GetTopTracks)
-	router.HandleFunc("/me/favorite/artists", artist.GetTopArtists)
-	router.HandleFunc("/me/playlists", playlist.GetPlaylists)
-
+	router.HandleFunc("/me/favorite/tracks", track.GetTopTracks).Methods("GET")
+	router.HandleFunc("/me/favorite/artists", artist.GetTopArtists).Methods("GET")
+	router.HandleFunc("/me/playlists", playlist.GetPlaylists).Methods("GET")
 	http.ListenAndServe(":8080", handlers.CORS()(router))
+}
+
+func accessControlMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT")
+		w.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type")
+		if r.Method == "OPTIONS" {
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
 }
