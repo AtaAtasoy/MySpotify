4137d772d1acada6e3ae2b29390b9d49fa8d439d
Ata Atasoy <h.ataatasoy@gmail.com>
2022-09-20 15:06:17 +0300

    Some styling

--- client/pages/views/components/Playlist.jsx
+++ client/pages/views/components/Playlist.jsx
@@ -22,43 +22,45 @@ export default function Playlist({name, tracks, image}) {
     const avgTempo = useRef(0) // BPM
 
     useEffect(() => {
-        avgPopularity.current = tracks.reduce((accumulator, current) => {return accumulator + current.popularity}, 0) / length
-        avgAcousticness.current = tracks.reduce((accumulator, current) => {return accumulator + current.acousticness}, 0) / length
-        avgDanceability.current = tracks.reduce((accumulator, current) => {return accumulator + current.danceability}, 0) / length
-        avgDuration.current = tracks.reduce((accumulator, current) => {return accumulator + current.duration_ms}, 0) / length
-        avgEnergy.current = tracks.reduce((accumulator, current) => {return accumulator + current.energy}, 0) / length
-        avgInstrumentalness.current = tracks.reduce((accumulator, current) => {return accumulator + current.instrumentalness}, 0) / length
+        avgPopularity.current = (tracks.reduce((accumulator, current) => {return accumulator + current.popularity}, 0) / length).toPrecision(2)
+        avgAcousticness.current = (tracks.reduce((accumulator, current) => {return accumulator + current.acousticness}, 0) / length).toPrecision(2)
+        avgDanceability.current = (tracks.reduce((accumulator, current) => {return accumulator + current.danceability}, 0) / length).toPrecision(2)
+        avgEnergy.current = (tracks.reduce((accumulator, current) => {return accumulator + current.energy}, 0) / length).toPrecision(2)
+        avgInstrumentalness.current = (tracks.reduce((accumulator, current) => {return accumulator + current.instrumentalness}, 0) / length).toPrecision(2)
+        avgDuration.current = (tracks.reduce((accumulator, current) => {return accumulator + current.duration_ms}, 0) / length) / 100
         avgTempo.current = tracks.reduce((accumulator, current) => {return accumulator + current.tempo}, 0) / length
     }, [tracks, length])
 
-
+    /**
+     * TODO: Fix floating point errors
+     * TODO: Progress bar view also for duration and tempo
+     * TODO: Column view for the circular progress bars
+     */
     return (
         <div className='playlist-container'>
             <Image alt='playlist-image' width={150} height={150} src={image.url} style={{"borderRadius": "50%"}}/>
             <h3>{name}</h3>
-            {/** Playlist may contain podcasts instead of tracks, thus have to check if it contains tracks}*/}
             {tracks ? <div className="playlist-info-text">
-                <div style={{ width: 100, height: 100 }}>
-                    <CircularProgressbar value={avgPopularity.current} minValue={0} maxValue={100} text={`Popularity\n${avgPopularity.current}`} styles={buildStyles({ textSize: '16px', pathColor: `rgb(33, 224, 101)`})}/>
+                <div style={{ width: 150, height: 150 }}>
+                    <CircularProgressbar value={(avgPopularity.current)} minValue={0} maxValue={100} text={`${avgPopularity.current}%`} styles={buildStyles({ textSize: '16px', textColor: '#21e065', pathColor: `rgb(33, 224, 101)`})}/>
                 </div>
-                <div style={{ width: 100, height: 100 }}>
-                    <CircularProgressbar value={avgAcousticness.current} minValue={0} maxValue={1} text={`Acousticness\n${avgAcousticness.current}`} styles={buildStyles({pathColor: `rgb(33, 224, 101)`})}/>
+                <div style={{ width: 150, height: 150 }}>
+                    <CircularProgressbar value={(avgAcousticness.current)} minValue={0} maxValue={1} text={`${avgAcousticness.current * 100}%`} styles={buildStyles({ textSize: '16px', textColor: '#21e065', pathColor: `rgb(33, 224, 101)`})}/>
                 </div>
-                <div style={{ width: 100, height: 100 }}>
-                    <CircularProgressbar value={avgDanceability.current} minValue={0} maxValue={1} text={`Danceability\n${avgDanceability.current}`} styles={buildStyles({pathColor: `rgb(33, 224, 101)`})}/>
+                <div style={{ width: 150, height: 150 }}>
+                    <CircularProgressbar value={(avgDanceability.current)} minValue={0} maxValue={1} text={`${avgDanceability.current * 100}%`} styles={buildStyles({ textSize: '16px', textColor: '#21e065', pathColor: `rgb(33, 224, 101)`})}/>
                 </div>
-                <div style={{ width: 100, height: 100 }}>
-                    <CircularProgressbar value={avgEnergy.current} minValue={0} maxValue={1} text={`Energy\n${avgEnergy.current}`} styles={buildStyles({pathColor: `rgb(33, 224, 101)`})}/>
+                <div style={{ width: 150, height: 150 }}>
+                    <CircularProgressbar value={(avgEnergy.current)} minValue={0} maxValue={1} text={`${avgEnergy.current * 100}%`} styles={buildStyles({ textSize: '16px', textColor: '#21e065', pathColor: `rgb(33, 224, 101)`})}/>
                 </div>
-                <div style={{ width: 100, height: 100 }}>
-                    <CircularProgressbar value={avgInstrumentalness.current} minValue={0} maxValue={1} text={`Instrumentalness\n${avgInstrumentalness.current}`} styles={buildStyles({pathColor: `rgb(33, 224, 101)`})}/>
+                <div style={{ width: 150, height: 150 }}>
+                    <CircularProgressbar value={(avgInstrumentalness.current)} minValue={0} maxValue={1} text={`${avgInstrumentalness.current * 100}%`} styles={buildStyles({ textSize: '16px', textColor: '#21e065', pathColor: `rgb(33, 224, 101)`})}/>
                 </div>
                 <p>
-                    Duration: {avgDuration.current / 1000}<br/>
-                    Tempo: {avgTempo.current} bpm<br/>
+                    Duration: {Math.round(avgDuration.current)} seconds<br/>
+                    Tempo: {Math.round(avgTempo.current)} bpm<br/>
                 </p>
             </div> : <div className='podcast-playlist'/>}
-            {/**tracks ? tracks.map((trackData, i) => <Track key={i} {...trackData} />) : <div className='podcast-playlist'/> */}
         </div>
     )
 }
