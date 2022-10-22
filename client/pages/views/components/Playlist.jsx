d44cd03c37afc743e84000b8ba1f72547f3d571b
AtaAtasoy <h.ataatasoy@gmail.com>
2022-10-22 20:25:38 +0300

    update polar-area-chart

--- client/pages/views/components/Playlist.jsx
+++ client/pages/views/components/Playlist.jsx
@@ -18,6 +18,8 @@ export default function Playlist({ name, tracks, image }) {
     const avgDuration = useRef(0) // duration in milliseconds
     const avgEnergy = useRef(0) // 0 <= energy <= 1
     const avgInstrumentalness = useRef(0) // 0 <= avgInstrumentalness <= 1
+    const avgSpeechines = useRef(0)
+    const avgValence = useRef(0)
     const avgTempo = useRef(0) // BPM
 
     useEffect(() => {
@@ -26,6 +28,8 @@ export default function Playlist({ name, tracks, image }) {
         avgDanceability.current = ((tracks.reduce((accumulator, current) => { return accumulator + current.danceability }, 0) / length) * 100).toFixed(2)
         avgEnergy.current = ((tracks.reduce((accumulator, current) => { return accumulator + current.energy }, 0) / length) * 100).toFixed(2)
         avgInstrumentalness.current = ((tracks.reduce((accumulator, current) => { return accumulator + current.instrumentalness }, 0) / length) * 100).toFixed(2)
+        avgSpeechines.current = ((tracks.reduce((accumulator, current) => { return accumulator + current.speechiness }, 0) / length) * 100).toFixed(2)
+        avgValence.current = ((tracks.reduce((accumulator, current) => { return accumulator + current.valence }, 0) / length) * 100).toFixed(2)
         avgDuration.current = Math.round((tracks.reduce((accumulator, current) => { return accumulator + current.duration_ms }, 0) / length) / 1000)
         avgTempo.current = Math.round(tracks.reduce((accumulator, current) => { return accumulator + current.tempo }, 0) / length)
 
@@ -40,12 +44,13 @@ export default function Playlist({ name, tracks, image }) {
                 <Image alt='playlist-image' width={150} height={150} src={image ? image.url : "https://thispersondoesnotexist.com/image"} style={{ "borderRadius": "50%" }} />
                 <h3>{name ? name : "Playlist"}</h3>
                 <AttributesRadialAreaChart data={[
-                                                    { x: "Popularity", y: Number(avgPopularity.current)},
-                                                    { x: "Acousticness", y: Number(avgAcousticness.current) },
-                                                    { x: "Danceability", y: Number(avgDanceability.current) },
-                                                    { x: "Energy", y: Number(avgEnergy.current) },
-                                                    { x: "Instrumentalness", y: Number(avgInstrumentalness.current) }
-                                                ]} />
+                    { x: "Speechiness", y: Number(avgSpeechines.current) },
+                    { x: "Acousticness", y: Number(avgAcousticness.current) },
+                    { x: "Danceability", y: Number(avgDanceability.current) },
+                    { x: "Energy", y: Number(avgEnergy.current) },
+                    { x: "Instrumentalness", y: Number(avgInstrumentalness.current) },
+                    { x: "Valence", y: Number(avgValence.current) }
+                ]} />
             </div>
         )
     }
