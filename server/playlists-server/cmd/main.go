3e18f4907aadc9f403e50e067bc63d39f2dd612d
Ata Atasoy <h.ataatasoy@gmail.com>
2022-10-24 00:42:28 +0300

    add endpoint for health checks

--- server/playlists-server/cmd/main.go
+++ server/playlists-server/cmd/main.go
@@ -1,20 +1,27 @@
 package main
 
 import (
-	"playlists-server/playlists"
+	"fmt"
 	"log"
 	"net/http"
+	"playlists-server/playlists"
 
 	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
 	"github.com/rs/cors"
 )
 
+func rootHandler(w http.ResponseWriter, r *http.Request){
+	fmt.Fprintf(w, "Welcome to playlists-server")
+	w.WriteHeader(http.StatusOK)
+}
+
 func main() {
 	godotenv.Load(".env")
 
 	router := mux.NewRouter()
-
+	
+	router.HandleFunc("/", rootHandler)
 	router.HandleFunc("/playlists", playlists.GetPlaylists)
 
 	//TODO:Setup CORS access origin
