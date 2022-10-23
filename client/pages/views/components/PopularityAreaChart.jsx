7a826500a23bcf0f2ee077209319624f443996df
Ata Atasoy <h.ataatasoy@gmail.com>
2022-10-24 02:43:17 +0300

    null safety for areachart

--- client/pages/views/components/PopularityAreaChart.jsx
+++ client/pages/views/components/PopularityAreaChart.jsx
@@ -3,18 +3,18 @@ import { VictoryChart, VictoryBar, VictoryScatter } from "victory";
 
 export default function PopularityAreaChart({ tracks }) {
     const data = []
-
-    for (let i = 0; i < tracks.length; i++) {
-        const currentTrack = tracks[i]
-        data.push({ x: currentTrack.name, y: currentTrack.popularity })
+    if (tracks){
+        for (let i = 0; i < tracks.length; i++) {
+            const currentTrack = tracks[i]
+            data.push({ x: currentTrack.name, y: currentTrack.popularity })
+        }
+        return (
+            <div className="popularity-area-chart">
+                <VictoryChart horizontal maxDomain={{ y: 100 }}>
+                    <VictoryBar data={data} />
+                    <VictoryScatter data={data} />
+                </VictoryChart>
+            </div>
+        )
     }
-    return (
-        <div className="popularity-area-chart">
-            <VictoryChart horizontal maxDomain={{ y: 100 }}>
-                <VictoryBar data={data} />
-                <VictoryScatter data={data} />
-            </VictoryChart>
-        </div>
-    )
-
 }
